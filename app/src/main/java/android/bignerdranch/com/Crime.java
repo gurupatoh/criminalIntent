6307cad46cdd1f925c3e57a8459de8cd42a59780
gurupatoh <patrickn0023@gmail.com>
2019-06-23 05:33:55 -0700

    Initial commit

---
+++ app/src/main/java/android/bignerdranch/com/Crime.java
@@ -0,0 +1,45 @@
+package android.bignerdranch.com;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class Crime {
+    //model layer
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private Boolean mSolved;
+    public  Crime(){
+        mId=UUID.randomUUID();
+        mDate= new Date();
+
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public Boolean getSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(Boolean solved) {
+        mSolved = solved;
+    }
+}
